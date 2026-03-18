168cbc9a25eb8084affe8a710258acd35a9e536d
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2026-03-18 09:19:38 +0500

    Add DNS server configuration for MongoDB connection

--- server/src/db/index.db.js
+++ server/src/db/index.db.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 import logger from "../../logger/winston.logger.js";
 
+import dns from 'node:dns';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
 const DB_NAME = 'hospital_appointment_system';
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`);
         logger.info(`🍀 MongoDB connected! DB HOST: ${connectionInstance.connection.host}`);
 
     } catch (error) {
