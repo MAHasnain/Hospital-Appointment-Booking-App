7d78f35ac145832fd1c59f204929cd48426f2219
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2026-03-28 09:55:28 +0500

    db connection refactor

--- server/src/db/index.db.js
+++ server/src/db/index.db.js
@@ -1,19 +1,34 @@
 import mongoose from "mongoose";
 import logger from "../../logger/winston.logger.js";
 
-import dns from 'node:dns';
-dns.setServers(['8.8.8.8', '1.1.1.1']);
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-const DB_NAME = 'hospital_appointment_system';
+const DB_NAME = "hospital_appointment_system";
+
+let isConnected = false;
+
 const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`);
-        logger.info(`🍀 MongoDB connected! DB HOST: ${connectionInstance.connection.host}`);
-
-    } catch (error) {
-        logger.error(`❌ MONGODB connection error: ${error}`);
-        process.exit(1);
+    if (isConnected) {
+        logger.info("⚡ Using existing DB connection");
+        return;
     }
-}
 
-export default connectDB
+    try {
+        const conn = await mongoose.connect(
+            `${process.env.MONGODB_URI}${DB_NAME}`,
+            {
+                serverSelectionTimeoutMS: 5000,
+            }
+        );
+
+        isConnected = conn.connections[0].readyState === 1;
+
+        logger.info(`🍀 MongoDB connected! HOST: ${conn.connection.host}`);
+    } catch (error) {
+        logger.error(`❌ MONGODB connection error: ${error.message}`);
+        throw error; // ❗ NOT process.exit
+    }
+};
+
+export default connectDB;
