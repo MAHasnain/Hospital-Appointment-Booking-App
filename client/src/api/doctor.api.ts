0193b2817cccffa6d372c4e11c107f7916ace3ca
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2026-04-05 13:12:01 +0500

    refactor(doctorAPI): update and complete doctor apiLayer code

--- client/src/api/doctor.api.ts
+++ client/src/api/doctor.api.ts
@@ -1,6 +1,6 @@
 import { editDoctorProfilePayload } from "@/types/users";
 import apiClient from ".";
 
-export const getDoctors = () => apiClient.get("/doctors");
-export const getDoctorById = (id: string) => apiClient.get(`/doctors/${id}`);
-export const editDoctorProfile = (data: editDoctorProfilePayload) => apiClient.put(`/doctors/profile`, data);
+export const getDoctorsApi = () => apiClient.get("/doctors");
+export const getDoctorByIdApi = (id: string) => apiClient.get(`/doctors/${id}`);
+export const editDoctorProfileApi = (data: editDoctorProfilePayload) => apiClient.put(`/doctors/profile`, data);
